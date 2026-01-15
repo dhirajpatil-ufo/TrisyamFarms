2d483297ecdddfa2c9a78bf1df9bf42d4dbdfc9d
dhiraj.patil <dhiraj.patil@UFODI-DHIRAJ>
2026-01-15 16:21:02 +0530

    new changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    provideClientHydration(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
